f77c44657d72fdca9b973a51a276222c48c494dc
sonik22 <klaus.loehmann@gmail.com>
2018-10-15 14:26:14 +0200

    Update spoolomatic.ino

--- spoolomatic.ino
+++ spoolomatic.ino
@@ -1,1 +1,12 @@
+// SPOOL-O-MATIC
+// setup: defining the wished diameter: 1,75, 2,85, 3,00 .....
 
+
+// 1st part: measuring luminosity with ler and translate it into understandable values (map-function)
+// creating variable to display value
+
+
+// 2nd part: reading the diameter value and tune the pull-motor with this value
+
+// 3rd part: turning the trolley motor in accordance to wished diameter
+// We need a startbutton to start movement of the trolley, once the diameter is established
