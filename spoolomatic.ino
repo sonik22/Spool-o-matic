54074b104ff28b7ca1fe06f4fdd458d8d455f349
sonik22 <klaus.loehmann@gmail.com>
2018-10-22 21:38:15 +0200

    Update spoolomatic.ino

--- spoolomatic.ino
+++ spoolomatic.ino
@@ -11,4 +11,9 @@
 // 2nd part: reading the diameter value and tune the pull-motor with this value
 
 // 3rd part: turning the trolley motor in accordance to wished diameter
+//
+// M8 has a pitch of 1.25mm. For a diametre of 2.85 and a speedwheel with 8 holes that means
+// 2.85 / 1.25 = 2.28 / 8 = 0;285 so every time a hole is detected, turn the axe 0.285 turns.
+// which means 0,285÷0,015625(turns per step)= 18.24, so make 18 steps every time a hole is detected. 
+// 
 // We need a startbutton to start movement of the trolley, once the diameter is established
