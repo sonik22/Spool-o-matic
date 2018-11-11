36011a345ba846d279717a1c2e8367a29c1357b9
sonik22 <klaus.loehmann@gmail.com>
2018-11-11 10:38:39 +0100

    Update spoolomatic.ino

--- spoolomatic.ino
+++ spoolomatic.ino
@@ -6,17 +6,50 @@
 // and calibrating the led luminosity to display diametre (actually the calibration is totally independant of the STM)
 // add Routine parkposition for trolley NO this will be done manually
 //
-// SETUP DISPLAY:
-#include <TFT.h>  // Arduino LCD library
+
+#include <Wire.h>
 #include <SPI.h>
+#include <TFT.h>
+#include <Encoder.h>
+#include <Adafruit_MotorShield.h>
 
+// Pindefinitions
+// Pins for TFT Screen D11=MOSI D13=SCK
+#define cs   10
+#define dc   9
+#define rst  8
 
-// SPI Pin definition for the STM32 
-#define cs   PB11
-#define dc   PB1
-#define rst  PB10
+// Creating Objects
+// Create the motor shield object with the default I2C address
+// I2C Pins are A4, A5
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+
+Adafruit_StepperMotor *pullMotor = AFMS.getStepper(64, 1);
+Adafruit_StepperMotor *slideMotor = AFMS.getStepper(64, 2);
+
+Encoder myEnc(2, 3);
+
 TFT TFTscreen = TFT(cs, dc, rst);
-//
+
+// Declaring variables
+
+int encSwitch = 4;
+int leftLimit = 6;
+int rightLimit = 7;
+int speedWheel = A2;
+int aInValue = A3;
+int spool = 12;
+
+
+int slideSpeed;
+float diameterArray[3] = { 1.75, 2.85, 3.00};
+int ldrValueArray[3] = {102, 164, 198};
+int stepsPerHoleArray[3] = {20, 30, 40};
+float density = 1;
+int spoolSpeed;
+int slideDirection(FORWARD);
+
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -25,31 +58,84 @@ Serial.begin(9600);
   TFTscreen.setRotation(3);
   TFTscreen.background(0, 0, 0);
   
+void setup() {
+  //start serial connection
+  Serial.begin(9600);
 
+  // Pin Modes
+  pinMode(encSwitch, INPUT_PULLUP);  //Encoder Switch
+  pinMode(leftLimit, INPUT_PULLUP);  //Left Limit Switch
+  pinMode(rightLimit, INPUT_PULLUP);  //Right Limit Switch
+  pinMode(speedWheel, INPUT_PULLUP); //Optocoupler speedwheel
+  pinMode(spool, INPUT_PULLUP); //Start spooling
+
+  pullMotor->setSpeed(10);
+
+}
+
+    void loop() {
+  
 // MODULE DIAMETER:
 // Use encoder to dial in the wished diameter and put it into a variable,
 // push to confirm and pass to density, dial in the density of the plastic used and put it into a variable.
 // We need three digital inputs.
 // ----------------------------------------------------------------
+
+
+  // Choose diameter
+  // Preparation of the encoder
+
+  int oldPosition  = 1;
+  int diameterIndex = 1;
+
+  int newPosition = myEnc.read();
+  if (newPosition > oldPosition) {
+    oldPosition = oldPosition + 1;
+    oldPosition = newPosition;
+  }
+  if (newPosition > 3) {
+    newPosition = 3;
+  }
+
+  if (newPosition < oldPosition) {
+    oldPosition = oldPosition - 1;
+    oldPosition = newPosition;
+  }
+  if (newPosition < 1) {
+    newPosition = 1;
+  }
 //
 // 1st part: measuring luminosity with ldr and translate it into understandable values (map-function)
 // Actually NO map-function: we will have one value on an analog input (0-1023) and we will use this value
 // to compare with the value hardcoded for each diameter
 // creating variable to display value
+      
+      
 
 // 2nd part: reading the diameter value and tune the pull-motor with this value, once the switch is switched from "manual" to "auto"
 // 
 // MODULE SPEED CONTROL:
 // We use a stepper motor to transport the filament, the speed is controlled by the value of an analog input
 // which is either the readout from the ldr (auto) or the potentiometer of (manual) control. 
-// We need four digital outs for the motor , two analog ins for the speed control and one digital in for 
-// the choice of auto/manual.
-// ----------------------------------------------------------------------
+  //*****************************************************************************//
+  // Retreive the LDR Value or Pot-Value on A3
+
+  int pullSpeed = analogRead(aInValue);
+  int refValue = ldrValueArray[diameterIndex];    // reading from defined array
+
+  if (pullSpeed > refValue) {
+    pullSpeed = pullSpeed -1;
+    }
+
+ if (pullSpeed < refValue) {
+    pullSpeed = pullSpeed +1;
+    }
+
+
 //
 // 3rd part: turning the trolley motor in accordance to wished diameter
 //We change the switch state to "start" to begin the counter, the spooling and the trolley-movement
 //
-
 // MODULE TROLLEY:
 // A stepper motor moves the trolley forth and back between two limit-switches, the speed is controlled by the result of
 // the speed-wheel (wheel with 8 holes on the axis and an optocoupleur) and the variable of the defined diameter.
@@ -61,3 +147,27 @@ Serial.begin(9600);
 // 2.85 / 1.25 = 2.28 / 8 = 0;285 so every time a hole is detected, turn the axe 0.285 turns.
 // which means 0,285÷0,015625(turns per step)= 18.24, so make 18 steps every time a hole is detected. 
 // 
+ //*****************************************************************************//
+
+  diameterIndex = newPosition;
+  float stepsPerHole = stepsPerHoleArray[diameterIndex];
+
+  if (digitalRead(spool) == LOW) {
+ // pullSpeed = map(pullSpeed, 0, 1023, 0, 100);  // determines pullSpeed between 0 and 100 rpm's
+
+    int leftState = digitalRead (leftLimit);
+    int rightState = digitalRead (rightLimit);
+
+    int holeDetect = digitalRead (speedWheel);
+
+    if (rightState = LOW) {
+      slideDirection = BACKWARD;
+    }
+    if (leftState = LOW) {
+      slideDirection = FORWARD;
+    }
+
+    if (holeDetect = LOW) {
+      slideMotor->step(stepsPerHole, slideDirection, SINGLE);
+    }
+  }
